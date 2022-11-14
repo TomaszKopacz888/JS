aa7ff1c27ea43561f2ab9f88877e313f08cd5e2b
Tomasz Kopacz <tomasz.kopacz@traxelektronik.pl>
2022-11-14 17:27:26 +0100

    lab4 ready

--- 4/script.js
+++ 4/script.js
@@ -7,91 +7,94 @@ const showNotes = () => {
     const jsonNotes = localStorage.getItem('notes');
     if (jsonNotes !== null) {
         notes = JSON.parse(jsonNotes);
-        const important=[];
-        const notImportant=[];
-        for (let i=0; i<notes.length;i++){
-            if (notes[i].pin===true) important.push(notes[i]);
-            else notImportant.push(notes[i]);
+        const important = [];
+        const notImportant = [];
+        for (let i = 0; i < notes.length; i++) {
+            if (notes[i].pin === true) important[i]=notes[i];
+            else notImportant[i]=notes[i];
         }
-        console.log(important)
-        console.log(notImportant)
-        renderNotes(important)
-        //renderNotes(notImportant)
+        console.log(important);
+        renderNotes(important);
+        renderNotes(notImportant);
     }
 }
 
-    const renderNotes=arr=>{
+const renderNotes = arr => {
+    if (arr.length !== 0) {
         for (let i = 0; i < arr.length; i++) {
-            const divNote = prepareNote(i)
-            notesDiv.appendChild(divNote);
+            if(arr[i]) {
+                const divNote = prepareNote(i)
+                console.log(divNote);
+                notesDiv.appendChild(divNote);
+            }
         }
-
     }
 
-    const prepareNote = (i) => {
-        const divNote = document.createElement('div');
-        divNote.classList.add('note');
-        const divDescription = document.createElement('div');
-        divDescription.classList.add('description');
-        const pin = document.createElement('div');
-        pin.innerHTML = '&#128392;';
-        pin.id=i;
-        pin.classList.add('pin');
+}
 
-        if (notes[i].pin === true) {
-            pin.classList.add('clickedPin');
-        }
+const prepareNote = (i) => {
+    const divNote = document.createElement('div');
+    divNote.classList.add('note');
+    const divDescription = document.createElement('div');
+    divDescription.classList.add('description');
+    const pin = document.createElement('div');
+    pin.innerHTML = '&#128392;';
+    pin.id = i;
+    pin.classList.add('pin');
 
-
-        const date = new Date(notes[i].date);
-        divNote.innerHTML = date.toLocaleString() + '<br><br>Title: ' + notes[i].title + '<br><br>';
-        divNote.appendChild(pin);
-        divNote.style.backgroundColor = notes[i].color;
-        divDescription.innerHTML = notes[i].description;
-        divNote.appendChild(divDescription);
-        return divNote;
+    if (notes[i].pin === true) {
+        pin.classList.add('clickedPin');
     }
 
-    const addNewNote = () => {
-        console.log(notes);
-        const title = document.querySelector('#title').value;
-        const description = document.querySelector('#description').value;
-        const color = document.querySelector('#color').value;
-        const pin = false;
-        const date = Date.now();
-        const note = {
-            'title': title,
-            'description': description,
-            'color': color,
-            'pin': pin,
-            'date': date
-        }
-        if (title.value !== '') {
-            notes.push(note);
-            localStorage.setItem('notes', JSON.stringify(notes));
-            location.reload();
-        } else result.innerHTML = 'Title can\'t be null';
+
+    const date = new Date(notes[i].date);
+    divNote.innerHTML = date.toLocaleString() + '<br><br>Title: ' + notes[i].title + '<br><br>';
+    divNote.appendChild(pin);
+    divNote.style.backgroundColor = notes[i].color;
+    divDescription.innerHTML = notes[i].description;
+    divNote.appendChild(divDescription);
+    return divNote;
+}
+
+const addNewNote = () => {
+    console.log(notes);
+    const title = document.querySelector('#title').value;
+    const description = document.querySelector('#description').value;
+    const color = document.querySelector('#color').value;
+    const pin = false;
+    const date = Date.now();
+    const note = {
+        'title': title,
+        'description': description,
+        'color': color,
+        'pin': pin,
+        'date': date
     }
-
-    const clickPin = x => {
-        x.classList.toggle('clickedPin')
-        if (notes[x.id].pin===true){
-            notes[x.id].pin=false;
-        }
-        else {
-            notes[x.id].pin = true;
-
-        }
+    if (title.value !== '') {
+        notes.push(note);
         localStorage.setItem('notes', JSON.stringify(notes));
         location.reload();
+    } else result.innerHTML = 'Title can\'t be null';
+}
+
+const clickPin = x => {
+    x.classList.toggle('clickedPin')
+    if (notes[x.id].pin === true) {
+        notes[x.id].pin = false;
+    } else {
+        notes[x.id].pin = true;
+
     }
+    localStorage.setItem('notes', JSON.stringify(notes));
+    location.reload();
+}
 
 
 showNotes();
-    const pins = document.querySelectorAll('.pin');
-    submit.addEventListener('click', addNewNote);
-    for (let i = 0; i < pins.length; i++) {
-        pins[i].addEventListener('click', () => clickPin(pins[i]))
-    }
+const pins = document.querySelectorAll('.pin');
+submit.addEventListener('click', addNewNote);
+for (let i = 0; i < pins.length; i++) {
+    pins[i].addEventListener('click', () => clickPin(pins[i]))
+}
 
 
